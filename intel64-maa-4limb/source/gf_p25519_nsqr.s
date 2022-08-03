fc784a85ca40b5af28260cb549f0f790f1d02b57
Kaushik Nath <43860902+kn-cs@users.noreply.github.com>
2022-08-03 23:49:54 +0530

    Update gf_p25519_nsqr.s

--- intel64-maa-4limb/source/gf_p25519_nsqr.s
+++ intel64-maa-4limb/source/gf_p25519_nsqr.s
@@ -40,186 +40,191 @@
 .global gfp25519nsqr
 gfp25519nsqr:
 
-movq    %rsp, %r11
-subq    $64, %rsp
+movq    %rsp,%r11
+andq 	$-32,%rsp
+subq    $64,%rsp
 
-movq    %r11,  0(%rsp)
-movq    %r12,  8(%rsp)
-movq    %r13, 16(%rsp)
-movq    %r14, 24(%rsp)
-movq    %r15, 32(%rsp)
-movq    %rbx, 40(%rsp)
-movq    %rbp, 48(%rsp)
-movq    %rdi, 56(%rsp)
+movq    %r11,0(%rsp)
+movq    %r12,8(%rsp)
+movq    %r13,16(%rsp)
+movq    %r14,24(%rsp)
+movq    %r15,32(%rsp)
+movq    %rbx,40(%rsp)
+movq    %rbp,48(%rsp)
+movq    %rdi,56(%rsp)
 
-movq    0(%rsi),  %rbx
-movq    8(%rsi),  %rbp
-movq    16(%rsi), %rcx
-movq    24(%rsi), %rsi
+movq    0(%rsi),%rbx
+movq    8(%rsi),%rbp
+movq    16(%rsi),%rcx
+movq    24(%rsi),%rsi
 
-movq    %rdx, %rdi
+movq    %rdx,%rdi
 
-.START:
+.L:
 
-subq    $1, %rdi
-
-movq    %rsi, %rax
+movq    %rsi,%rax
 mulq    %rsi
-movq    %rax, %r12
-xorq    %r13, %r13
+movq    %rax,%r12
+movq    $0,%r13
 
-movq    $38, %rax
+movq    $38,%rax
 mulq    %rdx
-movq    %rax, %r14
-movq    %rdx, %r15
+movq    %rax,%r14
+movq    %rdx,%r15
 
-shlq    $1, %rsi
-
-movq    %rbp, %rax
+movq    %rbp,%rax
 mulq    %rsi
-movq    %rax, %r8
-xorq    %r9, %r9
-movq    %rdx, %r10
-xorq    %r11, %r11
+movq    %rax,%r8
+movq    $0,%r9
+movq    %rdx,%r10
+movq    $0,%r11
 
-movq    %rcx, %rax
+addq    %rax,%r8
+adcq    $0,%r9
+addq    %rdx,%r10
+adcq    $0,%r11
+
+movq    %rcx,%rax
 mulq    %rcx
-addq    %rax, %r8
-adcq    $0, %r9
-addq    %rdx, %r10
-adcq    $0, %r11
+addq    %rax,%r8
+adcq    $0,%r9
+addq    %rdx,%r10
+adcq    $0,%r11
 
-movq    %rcx, %rax
+movq    %rcx,%rax
 mulq    %rsi
-addq    %rax, %r10
-adcq    $0, %r11
-addq    %rdx, %r12
-adcq    $0, %r13
+addq    %rax,%r10
+adcq    $0,%r11
+addq    %rdx,%r12
+adcq    $0,%r13
 
-movq    $38, %rax
+
+addq    %rax,%r10
+adcq    $0,%r11
+addq    %rdx,%r12
+adcq    $0,%r13
+
+movq    $38,%rax
 mulq    %r10
-imul    $38, %r11, %r11
-movq    %rax, %r10
-addq    %rdx, %r11
+imul    $38,%r11,%r11
+movq    %rax,%r10
+addq    %rdx,%r11
 
-movq    $38, %rax
+movq    $38,%rax
 mulq    %r12
-imul    $38, %r13, %r13
-movq    %rax, %r12
-addq    %rdx, %r13
+imul    $38,%r13,%r13
+movq    %rax,%r12
+addq    %rdx,%r13
 
-movq    %rbx, %rax
+movq    %rbx,%rax
 mulq    %rsi
-addq    %rax, %r14
-adcq    $0, %r15
-addq    %rdx, %r8
-adcq    $0, %r9
+addq    %rax,%r14
+adcq    $0,%r15
+addq    %rdx,%r8
+adcq    $0,%r9
 
-movq    %rbp, %rax
+addq    %rax,%r14
+adcq    $0,%r15
+addq    %rdx,%r8
+adcq    $0,%r9
+
+movq    %rbp,%rax
 mulq    %rcx
-addq    %rax, %r14
-adcq    $0, %r15
-addq    %rdx, %r8
-adcq    $0, %r9
+addq    %rax,%r14
+adcq    $0,%r15
+addq    %rdx,%r8
+adcq    $0,%r9
 
-addq    %rax, %r14
-adcq    $0, %r15
-addq    %rdx, %r8
-adcq    $0, %r9
+addq    %rax,%r14
+adcq    $0,%r15
+addq    %rdx,%r8
+adcq    $0,%r9
 
-movq    $38, %rax
+movq    $38,%rax
 mulq    %r8
-imul    $38, %r9, %r9
-movq    %rax, %r8
-addq    %rdx, %r9
+imul    $38,%r9,%r9
+movq    %rax,%r8
+addq    %rdx,%r9
 
-movq    %rbx, %rax
+movq    %rbx,%rax
 mulq    %rbx
-addq    %rax, %r8
-adcq    $0, %r9
-addq    %rdx, %r10
-adcq    $0, %r11
+addq    %rax,%r8
+adcq    $0,%r9
+addq    %rdx,%r10
+adcq    $0,%r11
 
-movq    %rbx, %rax
+movq    %rbx,%rax
 mulq    %rbp
-addq    %rax, %r10
-adcq    $0, %r11
-addq    %rdx, %r12
-adcq    $0, %r13
+addq    %rax,%r10
+adcq    $0,%r11
+addq    %rdx,%r12
+adcq    $0,%r13
 
-addq    %rax, %r10
-adcq    $0, %r11
-addq    %rdx, %r12
-adcq    $0, %r13
+addq    %rax,%r10
+adcq    $0,%r11
+addq    %rdx,%r12
+adcq    $0,%r13
 
-movq    %rbx, %rax
+movq    %rbx,%rax
 mulq    %rcx
-addq    %rax, %r12
-adcq    $0, %r13
-addq    %rdx, %r14
-adcq    $0, %r15
+addq    %rax,%r12
+adcq    $0,%r13
+addq    %rdx,%r14
+adcq    $0,%r15
 
-addq    %rax, %r12
-adcq    $0, %r13
-addq    %rdx, %r14
-adcq    $0, %r15
+addq    %rax,%r12
+adcq    $0,%r13
+addq    %rdx,%r14
+adcq    $0,%r15
 
-movq    %rbp, %rax
+movq    %rbp,%rax
 mulq    %rbp
-addq    %rax, %r12
-adcq    $0, %r13
-addq    %rdx, %r14
-adcq    $0, %r15
+addq    %rax,%r12
+adcq    $0,%r13
+addq    %rdx,%r14
+adcq    $0,%r15
 
-movq    %r10, %rbp
-addq    %r9, %rbp
-adcq    $0, %r11
+movq    %r10,%rbp
+addq    %r9,%rbp
+adcq    $0,%r11
 
-movq    %r12, %rcx
-addq    %r11, %rcx
-adcq    $0, %r13
+movq    %r12,%rcx
+addq    %r11,%rcx
+adcq    $0,%r13
 
-movq    %r14, %rsi
-addq    %r13, %rsi
-adcq    $0, %r15
+movq    %r14,%rsi
+addq    %r13,%rsi
+adcq    $0,%r15
 
-shld    $1, %rsi, %r15
-imul    $19, %r15, %r15
-andq    mask63, %rsi
+shld    $1,%rsi,%r15
+imul    $19,%r15,%rbx
+andq    mask63(%rip),%rsi
 
-addq    %r15, %r8
-adcq    $0, %rbp
-adcq    $0, %rcx
-adcq    $0, %rsi
+addq    %r8,%rbx
+adcq    $0,%rbp
+adcq    $0,%rcx
+adcq    $0,%rsi
 
-movq    $0, %rbx
-adcq    $0, %rbx
+subq    $1,%rdi
+cmpq    $0,%rdi
 
-shld	$1, %rsi, %rbx
-andq    mask63, %rsi
+jne     .L
 
-imul    $19, %rbx, %rbx
-addq    %r8, %rbx
+movq    56(%rsp),%rdi
 
-cmpq    $0, %rdi
+movq    %rbx,0(%rdi)
+movq    %rbp,8(%rdi)
+movq    %rcx,16(%rdi)
+movq    %rsi,24(%rdi)
 
-jne     .START
+movq    0(%rsp),%r11
+movq    8(%rsp),%r12
+movq    16(%rsp),%r13
+movq    24(%rsp),%r14
+movq    32(%rsp),%r15
+movq    40(%rsp),%rbx
+movq    48(%rsp),%rbp
 
-movq    56(%rsp), %rdi
-
-movq    %rbx,  0(%rdi)
-movq    %rbp,  8(%rdi)
-movq    %rcx, 16(%rdi)
-movq    %rsi, 24(%rdi)
-
-movq    0(%rsp),  %r11
-movq    8(%rsp),  %r12
-movq    16(%rsp), %r13
-movq    24(%rsp), %r14
-movq    32(%rsp), %r15
-movq    40(%rsp), %rbx
-movq    48(%rsp), %rbp
-
-movq    %r11, %rsp
+movq    %r11,%rsp
 
 ret
